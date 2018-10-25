9af77e25d37ec91d5de2bad44d8ae1a2b0ea5574
Gerom Gonzales <gonzalesgerom23@gmail.com>
2018-10-25 22:02:44 +0800

    Gerom Gonzales

---
+++ gonzales_e1.py
@@ -0,0 +1,12 @@
+datas = {"name": "Gerom D. Gonzales",
+         "spirit_animal": "Lion",
+         "reason": "I am strong and brave.",
+         "hobby": "play League of Legends",
+         "profession": "Game Developer"
+}
+
+print ("I am {}".format(datas["name"]))
+print ("My Spirit Animal is {}".format(datas["spirit_animal"]))
+print ("because {}".format(datas["reason"]))
+print ("When not in school,I love to {}".format(datas["hobby"]))
+print ("I dream of being a {}".format(datas["profession"])+ " in the future.")
---
+++ python.py
@@ -0,0 +1,8 @@
+data = {"name": "Gerom",
+        "age": 17,
+        "weight": 60.1,
+        "grades": [78,84,65,79]
+}
+#print(data)
+print("I am {}".format(data["name"]))
+print("My First Grade in English {}".format(data["grades"][0]))
