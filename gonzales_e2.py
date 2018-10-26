e455387f2a4529373ed6dfbc83a27ed7a2e2c05e
Gerom Gonzales <gonzalesgerom23@gmail.com>
2018-10-26 15:57:23 +0800

    Gerom Gonzales

--- gonzales_e2.py
+++ gonzales_e2.py
@@ -2,21 +2,23 @@ import sys
 
 sw = sys.argv[1]
 
+
 sw = float(sw)
+print("Your input: {}".format(sw)+" km/h")
     
 if sw >= 220.0:
         
-    print("Super Typhoon")
+    print("There's a Super Typhoon")
 
 elif sw >= 118.0:
-    print("Typhoon")
+    print("There's a Typhoon")
 
 elif sw >= 117.0:
-    print ("Severe Tropical Storm")
+    print ("There's a Severe Tropical Storm")
 
 elif sw >= 88.0:
-    print ("Tropical Storm")
+    print ("There's a Tropical Storm")
 
 else:
-    print("Tropical Depression")
+    print("There's a Tropical Depression")
 
--- gonzales_e3.py
+++ gonzales_e3.py
@@ -1,9 +1,2 @@
-import sys
-
-.input = sys.argv[1]
-
-.input = float(.input)
-
-print("You input {}".format(.input))
-
-sum_of_squares
+user_input =  input()
+print(user-input)
